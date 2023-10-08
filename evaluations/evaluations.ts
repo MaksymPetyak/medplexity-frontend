ff25c33c44eef0652b8d40923aa883e6a8601908
MaksymPetyak <petyak.mi@gmail.com>
2023-10-08 18:17:42 +0200

    Adds evaluation file with medmcqa on 50 questions

--- evaluations/evaluations.ts
+++ evaluations/evaluations.ts
@@ -20,7 +20,7 @@ export const DATASETS: { [datasetName: string]: Dataset } = {
     splits: {
       validation: {
         'gpt-4':
-          'https://raw.githubusercontent.com/MaksymPetyak/medplexity-frontend/main/evaluations/medmcqa_validation(10)_gpt-4_10.json',
+          'https://raw.githubusercontent.com/MaksymPetyak/medplexity-frontend/main/evaluations/medmcqa_validation(50)_gpt4_evaluation.json',
       },
     },
   },
