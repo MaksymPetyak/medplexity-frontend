57f9cacabebbc3e6a3df41761ba574da5c359107
MaksymPetyak <petyak.mi@gmail.com>
2023-10-08 15:42:58 +0200

    Adds tab select of prompt or prompt template

--- components/evaluation-viewer/prompt-card.tsx
+++ components/evaluation-viewer/prompt-card.tsx
@@ -1,4 +1,5 @@
 import { CollapsableCard } from '@/components/collapsable-card';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 interface PromptCardProps {
   prompt: string;
@@ -6,5 +7,20 @@ interface PromptCardProps {
 }
 
 export function PromptCard({ prompt, promptTemplate }: PromptCardProps) {
-  return <CollapsableCard title={'Prompt'}>{prompt}</CollapsableCard>;
+  return (
+    <CollapsableCard title={'Prompt'}>
+      <Tabs defaultValue="prompt">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="prompt">Final prompt</TabsTrigger>
+          {promptTemplate && (
+            <TabsTrigger value="template">Template</TabsTrigger>
+          )}
+        </TabsList>
+        <TabsContent value="prompt">{prompt}</TabsContent>
+        {promptTemplate && (
+          <TabsContent value="template">{promptTemplate}</TabsContent>
+        )}
+      </Tabs>
+    </CollapsableCard>
+  );
 }
