3736ba20d95ef025aa83db58ec602db93195a273
MaksymPetyak <petyak.mi@gmail.com>
2023-10-09 12:04:10 +0200

    Bring selector states under one useState hook

--- components/sidebar/evaluation-selector.tsx
+++ components/sidebar/evaluation-selector.tsx
@@ -9,53 +9,89 @@ import { Separator } from '@/components/ui/separator';
 import { ModelSelector } from '@/components/sidebar/model-selector';
 import useEvaluationStore from '@/lib/hooks/useEvaluationStore';
 
+interface SelectionState {
+  dataset: string;
+  split: SplitType;
+  model: string;
+}
+
 export function EvaluationSelector() {
   const { setEvaluationURL } = useEvaluationStore();
 
   const evals = DATASETS;
 
+  const [selections, setSelections] = useState<SelectionState>({
+    dataset: Object.keys(evals)[0],
+    split: 'train',
+    model: '',
+  });
+
   const availableDatasets = Object.keys(evals);
-  const [selectedDatasetKey, setSelectedDatasetKey] = useState(
-    availableDatasets[0],
-  );
-  const selectedDataset = evals[selectedDatasetKey];
-
+  const selectedDataset = evals[selections.dataset];
+  const availableSplits = Object.keys(selectedDataset.splits);
   // @ts-ignore
-  const availableSplits: SplitType[] = Object.keys(selectedDataset.splits);
-  const [selectedSplit, setSelectedSplit] = useState<SplitType>(
-    availableSplits[0],
-  );
-  const selectedSplitData = selectedDataset.splits[selectedSplit];
-
+  const selectedSplitData = selectedDataset.splits[selections.split];
   const availableModels = Object.keys(selectedSplitData || {});
-  const [selectedModel, setSelectedModel] = useState(availableModels[0]);
-
-  const evaluationURL = selectedSplitData
-    ? selectedSplitData[selectedModel]
-    : null;
 
   useEffect(() => {
+    // Reset splits and models when dataset changes
+    setSelections((prev) => ({
+      ...prev,
+      split: availableSplits[0] as SplitType,
+      model: availableModels[0],
+    }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selections.dataset]);
+
+  useEffect(() => {
+    // Reset models when split changes
+    setSelections((prev) => ({ ...prev, model: availableModels[0] }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selections.split]);
+
+  useEffect(() => {
+    const evaluationURL = selectedSplitData
+      ? selectedSplitData[selections.model]
+      : null;
     setEvaluationURL(evaluationURL);
-  }, [evaluationURL, setEvaluationURL]);
+  }, [selections.model, selectedSplitData, setEvaluationURL]);
+
+  // Step 3: Refactor into Helper Functions
+  const updateDataset = (newDataset: string) => {
+    setSelections({ dataset: newDataset, split: 'train', model: '' });
+  };
+
+  const updateSplit = (newSplit: string) => {
+    setSelections((prev) => ({
+      ...prev,
+      split: newSplit as SplitType,
+      model: '',
+    }));
+  };
+
+  const updateModel = (newModel: string) => {
+    setSelections((prev) => ({ ...prev, model: newModel }));
+  };
 
   return (
     <div className={'flex flex-col gap-2 mt-2 w-full'}>
       <DatasetSelector
-        value={selectedDatasetKey}
-        options={availableDatasets}
-        onValueChange={setSelectedDatasetKey}
+        value={selections.dataset}
+        options={Object.keys(evals)}
+        onValueChange={updateDataset}
       />
       <DataSplitSelector
-        value={selectedSplit}
+        value={selections.split}
+        // @ts-ignore
         options={availableSplits}
-        onValueChange={setSelectedSplit}
+        onValueChange={updateSplit}
       />
       <DatasetDescription description={selectedDataset.description} />
       <Separator className={'my-2 bg-gray-300'} />
       <ModelSelector
-        value={selectedModel}
+        value={selections.model}
         options={availableModels}
-        onValueChange={setSelectedModel}
+        onValueChange={updateModel}
       />
     </div>
   );
