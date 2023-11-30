72dfee92bd3edd7afdf3188497f8dfe25e7c6e72
MaksymPetyak <petyak.mi@gmail.com>
2023-11-30 03:22:00 +0100

    Add missing script for analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-41EME0V0WM" />
       <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
